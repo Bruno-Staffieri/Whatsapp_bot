556a1c233e71bbaf7f6af6d4f6336f8b14329c56
Bruno-Staffieri <brunostaffieri2015@gmail.com>
2025-12-22 21:16:51 -0300

    prueba 9

--- database.py
+++ database.py
@@ -1,24 +1,21 @@
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-DATABASE_URL = "sqlite:///whatsapp_scheduler.db"
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+DB_PATH = os.path.join(BASE_DIR, "whatsapp_scheduler.db")
 
 engine = create_engine(
-    DATABASE_URL,
+    f"sqlite:///{DB_PATH}",
     connect_args={"check_same_thread": False}
 )
 
-SessionLocal = sessionmaker(
-    autocommit=False,
-    autoflush=False,
-    bind=engine
-)
+SessionLocal = sessionmaker(bind=engine)
 
 Base = declarative_base()
 
 
 def init_db():
-    import models  # importante: evita imports circulares
     Base.metadata.create_all(bind=engine)
 
 
--- models.py
+++ models.py
@@ -6,13 +6,13 @@ class UserConfig(Base):
     __tablename__ = "user_config"
 
     id = Column(Integer, primary_key=True)
-    send_hour = Column(Integer, nullable=False)
-    send_minute = Column(Integer, nullable=False)
-    message = Column(String, nullable=False)
+    send_hour = Column(Integer, default=12)
+    send_minute = Column(Integer, default=0)
+    message = Column(String, default="Mensaje de prueba")
 
 
 class Recipient(Base):
     __tablename__ = "recipients"
 
     id = Column(Integer, primary_key=True)
-    phone = Column(String, nullable=False)
+    phone = Column(String, unique=True)
